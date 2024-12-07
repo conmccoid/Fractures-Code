2e6a4f6ecbbb367b74d1f17f9fff3377b04c44cb
conmccoid <conmccoid@gmail.com>
2024-12-06 21:29:45 -0500

    Fractures: fixed assignment in .Jn and .Fn, functional but not working vector created in .Fn needs to overwrite input F matrix input of .Jn now gets its values assigned in the method still not getting correct results also added line in tutorial on how to share folders with Docker volume need to change .vector to .x.petsc_vec

--- EX_GD_NewtonSolver.py
+++ EX_GD_NewtonSolver.py
@@ -6,7 +6,6 @@ from NewtonSolverContext import NewtonSolverContext
 class Identity:
     def mult(self, mat, X, Y):
         X.copy(Y)
-        print(f"multiplied!")
 
 class NewtonSolver:
     def __init__(self, solver1, solver2, problem1, problem2, A, B, C, D):
@@ -23,7 +22,7 @@ class NewtonSolver:
         # self.PJ=NewtonSolverContext(A, B, C, D, problem1, problem2) # preconditioned Jacobian
         self.PJ=Identity()
 
-    def Fn(self, snes, x, F):
+    def Fn(self, snes, x, F):        
         xu, xv = x.getNestSubVecs()
         xu.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
         xv.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
@@ -40,21 +39,17 @@ class NewtonSolver:
         self.solver2.solve(None, self.v.vector)
         self.v.x.scatter_forward() # should be unnecessary, depending on KSP in solver2
 
-        res_u = self.u.vector - self.u_old.vector
-        res_v = self.v.vector - self.v_old.vector
-        F = PETSc.Vec().createNest([res_u,res_v])
-        # F.assemble()
-        print(f"Residual: {F.norm():3.4e}")
-        F.ghostUpdate(addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
+        res_u = -self.u.vector + self.u_old.vector
+        res_v = -self.v.vector + self.v_old.vector
+        res = PETSc.Vec().createNest([res_u,res_v])
+        F.array[:] = res.array
 
         # self.v_old.x.array[:] = self.v.x.array
         # self.u_old.x.array[:] = self.u.x.array
 
     def Jn(self, snes, x, J, P):
-        J = PETSc.Mat().createPython(J.getSize())
         J.setPythonContext(self.PJ)
         J.setUp()
-        print(f"matrix set up!")
 
     def setUp(self):
         self.solver = PETSc.SNES().create(MPI.COMM_WORLD)
@@ -67,19 +62,7 @@ class NewtonSolver:
         b_v.setSizes(self.v.vector.getSize())
         b = PETSc.Vec().createNest([b_u,b_v])
         
-        J_uu = PETSc.Mat().create()
-        J_uu.setType('seqaij')
-        J_uu.setSizes(self.u.vector.getSizes())
-        J_vv = PETSc.Mat().create()
-        J_vv.setType('seqaij')
-        J_vv.setSizes(self.v.vector.getSizes())
-        J_uv = PETSc.Mat().create()
-        J_uv.setType('seqaij')
-        J_uv.setSizes([self.u.vector.getSize(), self.v.vector.getSize()])
-        J_vu = PETSc.Mat().create()
-        J_vu.setType('seqaij')
-        J_vu.setSizes([self.v.vector.getSize(), self.u.vector.getSize()])
-        J = PETSc.Mat().createNest([[J_uu,J_uv],[J_vu,J_vv]])
+        J = PETSc.Mat().createPython(self.u.vector.getSize()+self.v.vector.getSize())
 
         self.solver.setFunction(self.Fn, b)
         self.solver.setJacobian(self.Jn, J)
@@ -89,7 +72,7 @@ class NewtonSolver:
         self.solver.getKSP().setTolerances(rtol=1.0e-9)
         self.solver.getKSP().getPC().setType("none")
         opts=PETSc.Options()
-        opts['snes_linesearch_type']='basic'
+        opts['snes_linesearch_type']='none'
         self.solver.setFromOptions()
         self.solver.setMonitor(lambda snes, it, norm: print(f"Iteration {it}: Residual Norm = {norm:.6e}"))
-        # the residual is being calculated as zero, even when Fn() returns non-zero values
+        # no idea what's wrong now
