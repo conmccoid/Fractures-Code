de4d0c5b5fe48e37cc4cc8474e2f0e1de3a065ca
conmccoid <conmccoid@gmail.com>
2025-02-04 16:32:28 -0500

    working (?) circles example
    new EX file to run example
    updates to Domain file, incl. new BCs and VariationalFormulation methods and minor fixes to domain method

---
+++ EX_CTFM.py
@@ -0,0 +1,71 @@
+import numpy as np
+from mpi4py import MPI
+from petsc4py import PETSc
+from dolfinx import fem
+
+from EX_CTFM_Domain import domain, BCs, VariationalFormulation
+from EX_GD_Solvers import Elastic, Damage, Newton, alternate_minimization, AMEN
+from EX_GD_Visualization import plot_damage_state
+from EX_GD_NewtonSolver import NewtonSolver
+
+def main(method='AltMin'):
+    u, v, dom, cell_tags, facet_tags=domain()
+    V_u=u.function_space
+    V_v=v.function_space
+    bcs_u, bcs_v, t1, t2 = BCs(u,v,dom,cell_tags, facet_tags)
+    E_u, E_v, E_uu, E_vv, E_uv, E_vu, elastic_energy, dissipated_energy, load_c, = VariationalFormulation(u,v,dom,cell_tags,facet_tags)
+    
+    # now we want to solve E_u(u,v)=0 and E_v(u,v)=0 with alternate minimization with a Newton accelerator
+    # first set up solvers for the individual minimizations
+    
+    elastic_problem, elastic_solver = Elastic(E_u, u, bcs_u, E_uu)
+    damage_problem, damage_solver = Damage(E_v, v, bcs_v, E_vv)
+    v_lb =  fem.Function(V_v, name="Lower bound")
+    v_ub =  fem.Function(V_v, name="Upper bound")
+    v_lb.x.array[:] = 0.0
+    v_ub.x.array[:] = 1.0
+    damage_solver.setVariableBounds(v_lb.x.petsc_vec,v_ub.x.petsc_vec)
+    EN_solver = Newton(E_uv, E_vu, elastic_solver, damage_solver)
+    EN=NewtonSolver(elastic_solver, damage_solver,
+                    elastic_problem, damage_problem,
+                    E_uv, E_vu)
+    EN.setUp()
+    uv = PETSc.Vec().createNest([u.x.petsc_vec,v.x.petsc_vec])
+    
+    # Solving the problem and visualizing
+    import pyvista
+    from pyvista.utilities.xvfb import start_xvfb
+    start_xvfb(wait=0.5)
+    
+    # load_c = 0.19 * L  # reference value for the loading (imposed displacement)
+    loads = np.linspace(0, 1.5 * load_c * 1 / 10, 20) # (load_c/E)*L
+    
+    # Array to store results
+    energies = np.zeros((loads.shape[0], 3))
+    
+    for i_t, t in enumerate(loads):
+        t1.value = t
+        t2.value =-t
+        energies[i_t, 0] = t
+    
+        # Update the lower bound to ensure irreversibility of damage field.
+        v_lb.x.array[:] = v.x.array
+    
+        print(f"-- Solving for t = {t:3.2f} --")
+        if method=='AMEN':
+            AMEN(u,v, elastic_solver, damage_solver, EN_solver)
+        elif method=='NewtonLS':
+            EN.solver.solve(None,uv)
+        else:
+            alternate_minimization(u, v, elastic_solver, damage_solver)
+        plot_damage_state(u, v)
+    
+        # Calculate the energies
+        energies[i_t, 1] = MPI.COMM_WORLD.allreduce(
+            fem.assemble_scalar(fem.form(elastic_energy)),
+            op=MPI.SUM,
+        )
+        energies[i_t, 2] = MPI.COMM_WORLD.allreduce(
+            fem.assemble_scalar(fem.form(dissipated_energy)),
+            op=MPI.SUM,
+        )
--- EX_CTFM_Domain.py
+++ EX_CTFM_Domain.py
@@ -25,15 +25,72 @@ def sigma(u,v, nu,E,ndim): # stress tensor of damaged material (of disp & dmg)
 
 def domain():
     # Domain set-up
-    (mesh, cell_tags, facet_tags) = read_from_msh('CTFM.msh',MPI.COMM_WORLD)
-    ndim = mesh.geometry.dim
-    domain=mesh.ufl_domain()
+    (domain, cell_tags, facet_tags) = read_from_msh('CTFM.msh',MPI.COMM_WORLD)
 
     # Function space and solution initialization
-    element_u = basix.ufl.element("Lagrange", domain.ufl_cell(), degree=1, shape=(2,)) 
-    element_v = basix.ufl.element("Lagrange", domain.ufl_cell(), degree=1)
+    element_u = basix.ufl.element("Lagrange", domain.basix_cell(), degree=1, shape=(3,)) 
+    element_v = basix.ufl.element("Lagrange", domain.basix_cell(), degree=1)
     V_u = fem.functionspace(domain, element_u)
     V_v = fem.functionspace(domain, element_v)
     u = fem.Function(V_u, name="Displacement")
     v = fem.Function(V_v, name="Damage")
-    return u, v, domain
+    return u, v, domain, cell_tags, facet_tags
+
+def BCs(u,v,domain, cell_tags, facet_tags):
+    V_u=u.function_space
+    V_v=v.function_space
+    fdim=domain.topology.dim-1
+
+    circle1_facets=facet_tags.find(20)
+    circle2_facets=facet_tags.find(30)
+    crack_facets=facet_tags.find(40)
+
+    circle1_dofsx=fem.locate_dofs_topological(V_u.sub(0), fdim, circle1_facets)
+    circle2_dofsx=fem.locate_dofs_topological(V_u.sub(0), fdim, circle2_facets)
+    circle1_dofsy=fem.locate_dofs_topological(V_u.sub(1), fdim, circle1_facets)
+    circle2_dofsy=fem.locate_dofs_topological(V_u.sub(1), fdim, circle2_facets)
+    crack_dofs=fem.locate_dofs_topological(V_v, fdim, crack_facets)
+
+    t1=fem.Constant(domain, PETSc.ScalarType(1.0))
+    t2=fem.Constant(domain, PETSc.ScalarType(-1.0))
+    circle1_bcsx=fem.dirichletbc( t1, circle1_dofsx, V_u.sub(0))
+    circle2_bcsx=fem.dirichletbc( t2, circle2_dofsx, V_u.sub(0))
+    circle1_bcsy=fem.dirichletbc( t1, circle1_dofsy, V_u.sub(1))
+    circle2_bcsy=fem.dirichletbc( t2, circle2_dofsy, V_u.sub(1))
+    crack_bcs=fem.dirichletbc(fem.Constant(domain, PETSc.ScalarType(1.)), crack_dofs, V_v)
+
+    bcs_u=[circle1_bcsx, circle2_bcsx, circle1_bcsy, circle2_bcsy]
+    bcs_v=[crack_bcs]
+    return bcs_u, bcs_v, t1, t2
+
+def VariationalFormulation(u,v,domain,cell_tags,facet_tags):
+    V_u=u.function_space
+    V_v=v.function_space
+    ndim=domain.geometry.dim
+
+    # Variational formulation
+    E= fem.Constant(domain, PETSc.ScalarType(100.0))
+    nu= fem.Constant(domain, PETSc.ScalarType(0.3))
+
+    dx = ufl.Measure("dx",domain=domain, subdomain_data=cell_tags)
+    ds = ufl.Measure("ds",domain=domain, subdomain_data=facet_tags)
+
+    Gc=  fem.Constant(domain, PETSc.ScalarType(1.0))
+    ell= fem.Constant(domain, PETSc.ScalarType(0.1))
+    cw=  fem.Constant(domain, PETSc.ScalarType(1/2))
+    f =  fem.Constant(domain, PETSc.ScalarType((0.,0.,0.)))
+    load_c = np.sqrt(27 * Gc.value * E.value / (256 * ell.value) ) # AT2
+
+    elastic_energy = 0.5 * ufl.inner(sigma(u,v,nu,E,ndim), eps(u)) * dx
+    dissipated_energy= Gc/cw * ( w(v) / ell + ell * ufl.inner(ufl.grad(v), ufl.grad(v))) * dx
+    external_work=ufl.inner(f,u)*dx
+    total_energy=elastic_energy + dissipated_energy - external_work
+
+    E_u = ufl.derivative(total_energy, u, ufl.TestFunction(V_u))
+    E_uu= ufl.derivative(E_u,u,ufl.TrialFunction(V_u))
+    E_v = ufl.derivative(total_energy, v, ufl.TestFunction(V_v))
+    E_vv= ufl.derivative(E_v,v,ufl.TrialFunction(V_v))
+    E_uv= ufl.derivative(E_u,v,ufl.TrialFunction(V_v))
+    E_vu= ufl.derivative(E_v,u,ufl.TrialFunction(V_u))
+
+    return E_u, E_v, E_uu, E_vv, E_uv, E_vu, elastic_energy, dissipated_energy, load_c
