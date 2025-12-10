4a978fb2b45f9e5e4c57a2dd9ddd3752cd41ee3a
conmccoid <conmccoid@gmail.com>
2025-12-10 01:28:37 -0500

    minor fixes to EX2_CTFM
    unified energies array for AltMin and MSPIN
    identifier used in saving results

--- EX2_CTFM.py
+++ EX2_CTFM.py
@@ -14,27 +14,21 @@ def main(method='AltMin', linesearch=None, WriteSwitch=False, PlotSwitch=False):
     loads = np.linspace(0, 1.5 * fp.load_c * 12 / 10, 20) # (load_c/E)*L
     # first critical load is between 0.87 and 1.31 (but sometimes up to 1.7?)
     # second critical load between 4.79 and 5.22
-    if method=='AltMin':
-        energies = np.zeros((loads.shape[0], 5))  # Initialize energies array
-    else:
-        energies = np.zeros((loads.shape[0], 6))
+    energies = np.zeros((loads.shape[0], 6))  # Initialize energies array
 
     x, J = fp.createVecMat()  # Create empty vector and matrix
     res = x.duplicate()  # Create a duplicate for the residual vector
     p = x.duplicate()  # Create a duplicate for the search direction
 
     if method!='AltMin':
-        SNESKSP = KSPsetUp(fp, J, type="gmres", rtol=1.0e-5, max_it=100)  # Set up the KSP solver
+        SNESKSP = KSPsetUp(fp, J, type="gmres", rtol=1.0e-5, max_it=1000)  # Set up the KSP solver
 
     if WriteSwitch:
         with io.XDMFFile(fp.comm, f"output/EX_CTFM_{identifier}.xdmf","w") as xdmf:
             xdmf.write_mesh(fp.dom)
         with open(f"output/TBL_CTFM_{identifier}.csv",'w') as csv.file:
             writer=csv.writer(csv.file,delimiter=',')
-            if method=='AltMin':
-                writer.writerow(['t','Elastic energy','Dissipated energy','Total energy','Number of iterations'])
-            else:
-                writer.writerow(['t','Elastic energy','Dissipated energy','Total energy','Outer iterations', 'Inner iterations'])
+            writer.writerow(['t','Elastic energy','Dissipated energy','Total energy','Outer iterations', 'Inner iterations'])
 
     for i_t, t in enumerate(loads):
         fp.updateBCs(t)
@@ -78,9 +72,9 @@ def main(method='AltMin', linesearch=None, WriteSwitch=False, PlotSwitch=False):
                 x += v # update solution
             else:
                 SNESKSP.solve(res, p)  # Solve the linear system
+                energies[i_t,5]+=SNESKSP.getIterationNumber()
                 customLineSearch(res, p, type=linesearch, DBSwitch=DBTrick(res, p))
                 x += p  # Update the solution vector
-                energies[i_t,5]+=SNESKSP.getIterationNumber()
             fp.updateUV(x)
             error = fp.updateError()
             fp.monitor(iteration)
@@ -92,12 +86,12 @@ def main(method='AltMin', linesearch=None, WriteSwitch=False, PlotSwitch=False):
             fp.plot(x=x)
 
         if WriteSwitch:
-            with io.XDMFFile(fp.comm, f"output/EX_CTFM_{method}_{linesearch}.xdmf","a") as xdmf:
+            with io.XDMFFile(fp.comm, f"output/EX_CTFM_{identifier}.xdmf","a") as xdmf:
                 xdmf.write_function(fp.u, t)
                 xdmf.write_function(fp.v, t)
         
     if WriteSwitch:
-        with open(f"output/TBL_CTFM_{method}_{linesearch}.csv",'a') as csv.file:
+        with open(f"output/TBL_CTFM_{identifier}.csv",'a') as csv.file:
             writer=csv.writer(csv.file,delimiter=',')
             writer.writerows(energies)
 
