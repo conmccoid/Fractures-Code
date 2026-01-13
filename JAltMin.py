0dc02aae8bd77796665dfed6b4a36c959a7a3d6e
conmccoid <conmccoid@gmail.com>
2026-01-13 12:06:18 -0500

    Revert "working on debugs, going bad"
    This reverts commit f703034a39956a1d825a8455e05da33f23cceef9.

--- JAltMin.py
+++ JAltMin.py
@@ -23,13 +23,13 @@ class JAltMin:
     def getKSPs(self):
         ksp_uu=self.elastic_solver.getKSP()
         ksp_vv=self.damage_solver.getKSP()
-        # v_inactive=self.damage_solver.getVIInactiveSet() # need the inactive set when using VI Newton
+        v_inactive=self.damage_solver.getVIInactiveSet() # need the inactive set when using VI Newton
         opts=PETSc.Options()
         opts['ksp_reuse_preconditioner'] = True
         ksp_uu.setFromOptions()
         ksp_vv.setFromOptions()
         opts.destroy()
-        return ksp_uu, ksp_vv #, v_inactive
+        return ksp_uu, ksp_vv, v_inactive
     
     def resetKSPs(self, ksp):
         opts= PETSc.Options()
@@ -50,23 +50,15 @@ class JAltMin:
         self.Evu.multAdd(x1, y2, y2)
 
         # multiply by P
-        ksp_uu, ksp_vv = self.getKSPs()
+        ksp_uu, ksp_vv, v_inactive = self.getKSPs()
         ksp_uu.solve(y1, y1)
         self.Evu.multAdd(-y1,y2,y2)
-        # debug
-        # v_temp=ksp_vv.getSolution()
-        # v_lb, v_ub = self.damage_solver.getVariableBounds()
-        # inactive_count = ((v_temp > v_lb) & (v_temp < v_ub)).sum()
-        # if inactive_count < len(y2):
-        #     print(f"Warning: inactive count {inactive_count} less than vector length {len(y2)}")
-
-        ksp_vv.solve(y2, y2)
-        # # trying to restrict to inactive set in the ksp_vv.solve()
-        # y2_inactive=PETSc.Vec().create() # make sure this gets the same comm as y2
-        # y2.getSubVector(v_inactive, y2_inactive)
-        # ksp_vv.solve(y2_inactive, y2_inactive)
-        # y2.restoreSubVector(v_inactive, y2_inactive)
-        # y2_inactive.destroy()
-        # v_inactive.destroy()
+        # trying to restrict to inactive set in the ksp_vv.solve()
+        y2_inactive=y2.duplicate()
+        y2.getSubVector(v_inactive, y2_inactive)
+        ksp_vv.solve(y2_inactive, y2_inactive)
+        y2.restoreSubVector(v_inactive, y2_inactive)
+        y2_inactive.destroy()
+        v_inactive.destroy()
         self.resetKSPs(ksp_uu)
         self.resetKSPs(ksp_vv)
