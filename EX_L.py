e0a1ed19e7e1588c2419723a2ae7d4370e8c46ab
conmccoid <conmccoid@gmail.com>
2026-02-23 13:03:02 -0500

    EX2 -> EX
    deprecated previous EX files
    renamed all EX2 to EX
    EX_Surf needed reformulating in new workflow

--- EX_L.py
+++ EX_L.py
@@ -1,104 +1,23 @@
 import numpy as np
-from mpi4py import MPI
-from petsc4py import PETSc
-from dolfinx import fem, io
-
-import pyvista
-from pyvista.utilities.xvfb import start_xvfb
-import matplotlib.pyplot as plt
-import csv
-import sys
-
 from DOM_L import domain, BCs, VariationalFormulation
-from Solvers import Elastic, Damage, alternate_minimization
-from PLOT_DamageState import plot_damage_state
-from NewtonSolver import NewtonSolver
+from FPAltMin import FPAltMin
+from OuterSolver import OuterSolver
 
-def main(method='AltMin',linesearch='bt',PlotSwitch=False,WriteSwitch=False):
-    comm=MPI.COMM_WORLD
-    rank=comm.rank
-    rtol=1.0e-6
-    
-    u, v, dom =domain()
-    V_u=u.function_space
-    V_v=v.function_space
-    E_u, E_v, E_uu, E_vv, E_uv, E_vu, elastic_energy, dissipated_energy, p, total_energy = VariationalFormulation(u,v,dom)
-    bcs_u, bcs_v, uD = BCs(u,v,dom)
-    
-    # now we want to solve E_u(u,v)=0 and E_v(u,v)=0 with alternate minimization with a Newton accelerator
-    # first set up solvers for the individual minimizations
-    
-    elastic_problem, elastic_solver = Elastic(E_u, u, bcs_u, E_uu)
-    damage_problem, damage_solver = Damage(E_v, v, bcs_v, E_vv)
-    v_lb =  fem.Function(V_v, name="Lower bound")
-    v_ub =  fem.Function(V_v, name="Upper bound")
-    v_lb.x.array[:] = 0.0
-    v_ub.x.array[:] = 1.0
-    # damage_solver.setVariableBounds(v_lb.x.petsc_vec,v_ub.x.petsc_vec)
+class FP(FPAltMin):
+    def __init__(self):
+        self.u, self.v, self.dom=domain()
+        bcs_u, bcs_v, self.u_D = BCs(self.u,self.v,self.dom)
+        E_u, E_v, E_uu, E_vv, E_uv, E_vu, self.elastic_energy, self.dissipated_energy, p, self.total_energy = VariationalFormulation(self.u,self.v,self.dom)
+        self.setUp(E_u, E_v, E_uu, E_vv, E_uv, E_vu, bcs_u, bcs_v)
 
-    if method=='Newton':
-        EN=NewtonSolver(elastic_solver, damage_solver,
-                    elastic_problem, damage_problem,
-                    E_uv, E_vu,
-                    linesearch=linesearch)
-        EN.setUp(rtol=rtol,max_it_SNES=1000,max_it_KSP=100,ksp_restarts=100,monitor='off')
-        uv = PETSc.Vec().createNest([u.x.petsc_vec,v.x.petsc_vec])#,None,MPI.COMM_WORLD)
-    
-    # Solving the problem and visualizing
-    if PlotSwitch:
-        start_xvfb(wait=0.5)
-    
+    def updateBCs(self, t):
+        self.u_D.value = t
+
+def main(method='AltMin', WriteSwitch=False, PlotSwitch=False):
+    fp = FP()
+    example='L'
     loads = np.linspace(0,0.6,81)
-    
-    # Array to store results
-    energies = np.zeros((loads.shape[0], 5 ))
-    output=np.zeros((loads.shape[0],3))
 
-    if WriteSwitch:
-        with io.XDMFFile(dom.comm, f"output/EX_L_{method}_{linesearch}.xdmf",'w') as xdmf:
-            xdmf.write_mesh(dom)
-
-    for i_t, t in enumerate(loads):
-        uD.value=t
-        energies[i_t, 0] = t
-    
-        # Update the lower bound to ensure irreversibility of damage field.
-        v_lb.x.array[:] = v.x.array
-
-        if rank==0:
-            print(f"-- Solving for t = {t:3.2e} --")
-        if method=='Newton':
-            EN.solver.solve(None,uv)
-            energies[i_t,4] = EN.solver.getIterationNumber()
-            output[i_t,:] = [t,EN.output,EN.solver.getIterationNumber()]
-        else:
-            iteration = alternate_minimization(u, v, elastic_solver, damage_solver, rtol, 1000, monitor=True)
-            energies[i_t,4] = iteration
-            output[i_t,:]=[t,0,iteration]
-
-        if PlotSwitch:
-            plot_damage_state(u, v, None, [1400, 850])
-        
-        # Calculate the energies
-        energies[i_t, 1] = comm.allreduce(
-            fem.assemble_scalar(fem.form(elastic_energy)),
-            op=MPI.SUM,
-        )
-        energies[i_t, 2] = comm.allreduce(
-            fem.assemble_scalar(fem.form(dissipated_energy)),
-            op=MPI.SUM,
-        )
-        energies[i_t, 3] = comm.allreduce(
-            fem.assemble_scalar(fem.form(total_energy)),
-            op=MPI.SUM,
-        )
-        if WriteSwitch:
-            with io.XDMFFile(dom.comm, f"output/EX_L_{method}_{linesearch}.xdmf","a") as xdmf:
-                xdmf.write_function(u, t)
-                xdmf.write_function(v, t)
-    return output, energies
-
-if __name__ == "__main__":
-    pyvista.OFF_SCREEN=True
-    main('NewtonLS')
-    sys.exit()
+    os=OuterSolver(fp, example, method, loads)
+    os.solve(WriteSwitch=WriteSwitch, PlotSwitch=PlotSwitch, maxit=1000, tol=1e-6)
+    return os.energies, os.identifier
--- EX_Surf.py
+++ EX_Surf.py
@@ -1,115 +1,25 @@
 import numpy as np
-from mpi4py import MPI
-from petsc4py import PETSc
-from dolfinx import fem, io
-
-import pyvista
-from pyvista.utilities.xvfb import start_xvfb
-import matplotlib.pyplot as plt
-import csv
-import sys
-
 from DOM_Surf import domain, BCs, VariationalFormulation, SurfBC
-from Solvers import Elastic, Damage, alternate_minimization
-from PLOT_DamageState import plot_damage_state
-from NewtonSolver import NewtonSolver
+from FPAltMin import FPAltMin
+from OuterSolver import OuterSolver
 
-def main(method='AltMin'):
-    comm=MPI.COMM_WORLD
-    rank=comm.rank
-    
-    u, v, dom, cell_tags, facet_tags=domain()
-    V_u=u.function_space
-    V_v=v.function_space
-    E_u, E_v, E_uu, E_vv, E_uv, E_vu, elastic_energy, dissipated_energy, p, total_energy = VariationalFormulation(u,v,dom,cell_tags,facet_tags)
-    bcs_u, bcs_v, U, bdry_cells = BCs(u,v,dom,cell_tags, facet_tags, p)
-    
-    # now we want to solve E_u(u,v)=0 and E_v(u,v)=0 with alternate minimization with a Newton accelerator
-    # first set up solvers for the individual minimizations
-    
-    elastic_problem, elastic_solver = Elastic(E_u, u, bcs_u, E_uu)
-    damage_problem, damage_solver = Damage(E_v, v, bcs_v, E_vv)
-    v_lb =  fem.Function(V_v, name="Lower bound")
-    v_ub =  fem.Function(V_v, name="Upper bound")
-    v_lb.x.array[:] = 0.0
-    v_ub.x.array[:] = 1.0
-    # damage_solver.setVariableBounds(v_lb.x.petsc_vec,v_ub.x.petsc_vec)
-    EN=NewtonSolver(elastic_solver, damage_solver,
-                    elastic_problem, damage_problem,
-                    E_uv, E_vu)
-    EN.setUp(rtol=1.0e-4,max_it_SNES=1000,max_it_KSP=100,ksp_restarts=100)
-    uv = PETSc.Vec().createNest([u.x.petsc_vec,v.x.petsc_vec])#,None,MPI.COMM_WORLD)
-    
-    # Solving the problem and visualizing
-    start_xvfb(wait=0.5)
-    
-    loads = np.linspace(0.5,6,18)
-    
-    # Array to store results
-    energies = np.zeros((loads.shape[0], 5 ))
-    iter_count=[]
-    with open(f"output/TBL_Surf_{method}_energy.csv",'w') as csv.file:
-        writer=csv.writer(csv.file,delimiter=',')
-        writer.writerow(['t','Elastic energy','Dissipated energy','Total energy','Number of iterations'])
-    with io.XDMFFile(dom.comm, f"output/EX_Surf_{method}.xdmf","w") as xdmf:
-        xdmf.write_mesh(dom)
+class FP(FPAltMin):
+    def __init__(self):
+        '''Initialize the problem by defining the domain, variational formulation, and boundary conditions.
+        - p: problem parameters
+        '''
+        u, v, dom, cell_tags, facet_tags=domain()
+        E_u, E_v, E_uu, E_vv, E_uv, E_vu, self.elastic_energy, self.dissipated_energy, self.p, self.total_energy = VariationalFormulation(u,v,dom,cell_tags,facet_tags)
+        bcs_u, bcs_v, self.U, self.bdry_cells = BCs(u,v,dom,cell_tags, facet_tags, self.p)
+        self.setUp(E_u, E_v, E_uu, E_vv, E_uv, E_vu, bcs_u, bcs_v)
 
-    for i_t, t in enumerate(loads):
-        U.interpolate(lambda x: SurfBC(x,t,p),bdry_cells)
-        energies[i_t, 0] = t
-    
-        # Update the lower bound to ensure irreversibility of damage field.
-        v_lb.x.array[:] = v.x.array
+    def updateBCs(self, t):
+        self.U.interpolate(lambda x: SurfBC(x,t,self.p),self.bdry_cells)
 
-        if rank==0:
-            print(f"-- Solving for t = {t:3.2f} --")
-        if method=='NewtonLS':
-            EN.solver.solve(None,uv)
-            energies[i_t,4] = EN.solver.getIterationNumber()
-        else:
-            iter_count, iteration = alternate_minimization(u, v, elastic_solver, damage_solver, 1e-4, 1000, True, iter_count)
-            energies[i_t,4] = iteration
-        plot_damage_state(u, v, None, [1400, 850])
-        
-        # Calculate the energies
-        energies[i_t, 1] = comm.allreduce(
-            fem.assemble_scalar(fem.form(elastic_energy)),
-            op=MPI.SUM,
-        )
-        energies[i_t, 2] = comm.allreduce(
-            fem.assemble_scalar(fem.form(dissipated_energy)),
-            op=MPI.SUM,
-        )
-        energies[i_t, 3] = comm.allreduce(
-            fem.assemble_scalar(fem.form(total_energy)),
-            op=MPI.SUM,
-        )
-        if rank==0:
-            with open(f"output/TBL_Surf_{method}_energy.csv",'a') as csv.file:
-                writer=csv.writer(csv.file,delimiter=',')
-                writer.writerow(energies[i_t,:])
-        with io.XDMFFile(dom.comm, f"output/EX_Surf_{method}.xdmf","a") as xdmf:
-            xdmf.write_function(u, t)
-            xdmf.write_function(v, t)
-            xdmf.write_function(U, t)
-    with open(f"output/TBL_Surf_{method}_its.csv",'w') as csv.file:
-        writer=csv.writer(csv.file,delimiter=',')
-        if method=='NewtonLS':
-            writer.writerows(EN.output) # find some way to combine these files into one
-        elif method=='AltMin':
-            writer.writerows(iter_count)
-
-    fig, ax=plt.subplots()
-    ax.plot(energies[:,0],energies[:,1],label='elastic energy')
-    ax.plot(energies[:,0],energies[:,2],label='dissipated energy')
-    ax.plot(energies[:,0],energies[:,3],label='total energy')
-    ax.set_xlabel('t')
-    ax.set_ylabel('Energy')
-    ax.legend()
-    plt.savefig(f"output/FIG_Surf_{method}_energy.png")
-    # plt.show()
-
-if __name__ == "__main__":
-    pyvista.OFF_SCREEN=True
-    main('NewtonLS')
-    sys.exit()
+def main(method='AltMin', WriteSwitch=False, PlotSwitch=False):
+    fp = FP()
+    example = 'Surf'
+    loads = np.linspace(0.5, 6, 18)  # Load values
+    os = OuterSolver(fp, example, method, loads)
+    os.solve(WriteSwitch=WriteSwitch, PlotSwitch=PlotSwitch, maxit=1000, tol=1e-4)
+    return os.energies, os.identifier
