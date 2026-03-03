33ab0b8ead1a35abc5e4891ed4bf868ab0321095
conmccoid <conmccoid@gmail.com>
2026-03-03 18:09:47 -0500

    attempt to parallelize NX_GD
    plotNX() now runs only on rank 0
    comm.Barrier() should stop processes moving on before finishing on all ranks
    id_list and en_list are now formed only on rank 0

--- NX_GD.py
+++ NX_GD.py
@@ -1,19 +1,27 @@
 from EX_GD import main as EX
 from Utilities import plotNX
+from mpi4py import MPI
 import sys
 
 def main(id_list=['GD_AltMin','GD_CubicBacktracking','GD_Parallelogram'], en_list=None, WriteSwitch=True):
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
 
     if WriteSwitch:
         en1, id1=EX('AltMin',WriteSwitch=True)
+        comm.Barrier() # ensure all processes have finished writing before moving on
         en2, id2=EX('CubicBacktracking',WriteSwitch=True)
+        comm.Barrier()
         en3, id3=EX('Parallelogram',WriteSwitch=True)
+        comm.Barrier()
         # for linesearch in linesearch_list:
         #     EX('Newton',linesearch=linesearch,WriteSwitch=True)
-        id_list=[id1,id2,id3]
-        en_list=[en1,en2,en3]
+        if rank == 0:
+            id_list=[id1,id2,id3]
+            en_list=[en1,en2,en3]
     
-    plotNX('GD',id_list, en_list)
+    if rank == 0:
+        plotNX('GD',id_list, en_list)
 
 if __name__== "__main__":
     main()
