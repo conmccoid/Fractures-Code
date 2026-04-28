24f317532e6f1e8459e8a7c70aafa2d4ced216f1
conmccoid <conmccoid@gmail.com>
2026-04-28 10:56:23 -0400

    debug with EX_CTFM.sh
    problem computing an angle in parallelogram run of NX_CTFM

---
+++ EX_CTFM.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=EX_CTFM
+#SBATCH --output=output_CTFM.txt
+#SBATCH --error=error_CTFM.txt
+#SBATCH --partition=bb
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+#SBATCH --cpus-per-task=1
+#SBATCH --time=12:00:00
+#SBATCH --mail-user=mccoidc@mcmaster.ca
+#SBATCH --no-requeue
+
+srun apptainer exec fractures-code_dolfinx.sif python3 EX_CTFM.py --method Parallelogram
