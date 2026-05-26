98064f595bf10b6073d8a9b1fafb02849ba3852e
conmccoid <conmccoid@gmail.com>
2026-05-26 16:08:00 -0400

    Revert "start on edits for pm3"
    This reverts commit d3c3d3da41a300773aa38784c594e3a16ea8bfae.

--- GlobalizationTechniques.py
+++ GlobalizationTechniques.py
@@ -197,13 +197,25 @@ def pm3(fp, x, q, p, filename=None):
     [a,b,c,d,e,f,r,alpha,beta], [E0,Eq,Ep,Epq], qp = pbt(fp,x,q,p)
     qp.destroy()
 
-    if r>0 & a>0:
-        result=q.copy()
-        result.scale(alpha)
-        result.axpy(beta,p)
+    if r>0:
+        if a>0:
+            result=q.copy()
+            result.scale(alpha)
+            result.axpy(beta,p)
+        else:
+            # plotEnergyLandscape2D(fp,x,q,p,[beta,alpha],filename=filename)
+            # while Eq < E0:
+            #     E0=Eq
+            #     q.scale(2)
+            #     xq=x.copy()
+            #     xq.axpy(1,q)
+            #     Eq=fp.updateEnergies(xq)[2]
+            # q.scale(0.5)
+            # result=q.copy()
+            result=q.copy()
+            result.scale(alpha)
+            result.axpy(beta,p)
     else:
-        # change interpolant to include new information -- accuracy of derivative calculation in question
-        
         # alpha = -b
         # beta = (a-c) - np.sqrt((a-c)**2 + b**2)
         plotEnergyLandscape2D(fp,x,q,p,[beta, alpha],filename=filename, coeffs=[a,b,c,d,e,f])
